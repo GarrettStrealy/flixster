2b3125625f19908adcb3f300af972a391842409e
Garrett Strealy <gstrealy@gmail.com>
2021-02-26 21:48:57 -0600

    Superhero tab working

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Garrett Strealy on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
